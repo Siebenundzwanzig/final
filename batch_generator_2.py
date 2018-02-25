30269b581ffb43632daed566da314c683f71d5bb
Siebenundzwanzig <spiele-registrierer@web.de>
2018-02-25 16:33:14 +0100

    doch das von lukas umgeschrieben

--- batch_generator_2.py
+++ batch_generator_2.py
@@ -1,22 +1,29 @@
+# Typical setup to include TensorFlow.
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import os
 class ATTEND():
-    def __init__(self, directory = '/home/laars/uni/WS2017/tensorflow/final/glued_images'):
+    def __init__(self, directory = '/home/laars/uni/WS2017/tensorflow/final/glued_images/'):
         self._directory = directory
 
-        self._training_data = []
-        self._training_labels = []
-        self._validation_data = []
-        self._validation_labels = []
-        self._test_data = []
-        self._test_labels = []
+        self._training_data = np.array([])
+        self._training_labels = np.array([])
+        self._validation_data = np.array([])
+        self._validation_labels = np.array([])
+        self._test_data = np.array([])
+        self._test_labels = np.array([])
 
         self._load_traing_data()
         self._load_validation_data()
         self._load_test_data()
+
+
+        self._training_data = self._training_data.reshape((self._training_labels.shape[0], 45, 225, 3))
+        # self._training_data = np.asarray(self._training_data)
+        # self._training_labels = np.array(self._training_labels)
+
 
         # np.random.seed(0)
         # samples_n = self._training_labels.shape[0]
@@ -32,24 +39,27 @@ class ATTEND():
     def _load_traing_data(self):
         for _, _, files in os.walk(self._directory + "training"):
             for image in files:
-                print(image)
-                self._training_data.append(np.asarray(Image.open(self._directory + "training" + image)))
-                self._training_labels.append(np.array(image.replace(".jpg", "")))
+                # print(image)
+                self._training_data = np.append(self._training_data,
+                    np.asarray(Image.open(self._directory + "training/" + image)))
+                self._training_labels = np.append(self._training_labels,
+                    np.array(image.replace(".jpg", "")))
 
+        # print(len(self._training_data[0][0][0]))
     def _load_validation_data(self):
         for _, _, files in os.walk(self._directory + "validation"):
             for image in files:
-                print(image)
-                self._training_data.append(np.asarray(Image.open(self._directory + "validation" + image)))
-                self._training_labels.append(np.array(image.replace(".jpg", "")))
+                # print(image)
+                self._validation_data = np.append(self._validation_data, np.asarray(Image.open(self._directory + "validation/" + image)))
+                self._validation_labels = np.append(self._validation_labels, np.array(image.replace(".jpg", "")))
 
 
     def _load_test_data(self):
         for _, _, files in os.walk(self._directory + "testing"):
             for image in files:
-                print(image)
-                self._training_data.append(np.asarray(Image.open(self._directory + "testing" + image)))
-                self._training_labels.append(np.array(image.replace(".jpg", "")))
+                # print(image)
+                self._test_data = np.append(self._test_data, np.asarray(Image.open(self._directory + "testing/" + image)))
+                self._test_labels = np.append(self._test_labels, np.array(image.replace(".jpg", "")))
 
     def get_training_batch(self, batch_size):
         return self._get_batch(self._training_data, self._training_labels, batch_size)
@@ -62,6 +72,7 @@ class ATTEND():
 
     def _get_batch(self, data, labels, batch_size):
         samples_n = labels.shape[0]
+        print(samples_n)
         if batch_size <= 0:
             batch_size = samples_n
 
@@ -79,3 +90,21 @@ class ATTEND():
         validation_samples_n = self._validation_labels.shape[0]
         test_samples_n = self._test_labels.shape[0]
         return training_samples_n, validation_samples_n, test_samples_n
+
+if __name__ == "__main__":
+    # for _, _, files in os.walk("/home/laars/uni/WS2017/tensorflow/final/glued_images" + "training"):
+    #     for image in files:
+    #         print(image)
+
+
+    attend = ATTEND()
+    sample = attend.get_training_batch(5)
+    fig = plt.figure()
+    img = next(sample)
+    for i in range(5):
+        #img=next(sample)
+        label=img[1][i]
+        ax=fig.add_subplot(150+i+1)
+        ax.set_title("label: " + str(label))
+        imgplot = ax.imshow(img[0][i])
+    plt.show()
