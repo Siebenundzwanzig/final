3a20cf9523ad09686d81e958fc34ef38ce8157fa
Siebenundzwanzig <spiele-registrierer@web.de>
2018-03-06 21:55:51 +0100

    final cleanup

--- batch_generator_3.py
+++ batch_generator_3.py
@@ -1,21 +1,25 @@
-# Typical setup to include TensorFlow.
+# Generator for batches
+# Very similar to the CIFAR batch generator of second homework.
+# Redesigned for our purposes.
+# Thank you, Lukas ;)
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import os
+
 class Attend():
+    # Initialize with given directory, load numpy files as training, validation
+    # and test data and labels
     def __init__(self, directory = '/home/laars/uni/WS2017/tensorflow/final/glued_images/'):
         self._directory = directory
-
         self._training_data = np.load(directory + 'training_data.npy')
         self._training_labels = np.load(directory + 'training_labels.npy')
         self._validation_data = np.load(directory + 'validation_data.npy')
         self._validation_labels = np.load(directory + 'validation_labels.npy')
         self._test_data = np.load(directory + 'testing_data.npy')
         self._test_labels = np.load(directory + 'testing_labels.npy')
-
-        # print("training", self._training_data.shape)
 
     def get_training_batch(self, batch_size):
         return self._get_batch(self._training_data, self._training_labels, batch_size)
@@ -26,9 +30,10 @@ class Attend():
     def get_test_batch(self, batch_size):
         return self._get_batch(self._test_data, self._test_labels, batch_size)
 
+    # Returns a generator with all data and labels in random order and
+    # as many as wished.
     def _get_batch(self, data, labels, batch_size):
         samples_n = labels.shape[0]
-        # print(samples_n)
         if batch_size <= 0:
             batch_size = samples_n
 
@@ -40,25 +45,22 @@ class Attend():
             off = on + batch_size
             yield data[on:off], labels[on:off]
 
-
+    # returns number of data
     def get_sizes(self):
         training_samples_n = self._training_labels.shape[0]
         validation_samples_n = self._validation_labels.shape[0]
         test_samples_n = self._test_labels.shape[0]
         return training_samples_n, validation_samples_n, test_samples_n
 
+# If called as main, show with matplotlib.pyplot.
+# Used for debugging
 if __name__ == "__main__":
-
     attend = Attend()
-    # print("get_sizes: ", attend.get_sizes())
     sample = attend.get_training_batch(5)
     print(attend.get_sizes())
     fig = plt.figure()
     img = next(sample)
     for i in range(5):
-        # print(img[1][i])
-        # print(img[0][i].shape)
-        #img=next(sample)
         label=img[1][i]
         ax=fig.add_subplot(150+i+1)
         ax.set_title("label: " + str(label))
